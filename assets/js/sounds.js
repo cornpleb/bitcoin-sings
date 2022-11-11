9118a2c03e972c3ebe27e7adf3bf26487510002b
Cornpleb <cornpleb@bitcoindata.com>
2022-11-11 10:33:33 +0100

    Add Howler.js

---
+++ assets/js/sounds.js
@@ -0,0 +1,20 @@
+import { Howl, Howler } from '../vendor/howler'
+
+
+export var Sounds = {
+
+  play: function () {
+
+    var sound = new Howl({
+      src: ['/assets/sounds/flute.wav'],
+      html5: true
+    })
+
+    sound.play()
+  }
+}
+
+
+window.addEventListener("click", function () {
+  Sounds.play()
+})
